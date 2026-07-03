e12f2e8f45858c355f8f93b6452d7a9d1b194e90
marie <mariecaron27@gmail.com>
2026-07-03 09:05:34 +0200

    Add a second window

--- MainWindow.cs
+++ MainWindow.cs
@@ -9,8 +9,11 @@ namespace file_XIB;
 public partial class MainWindow : NSWindowController
 {
     private int numberOfTimesClicked = 0;
+    private SubViewType ViewType = SubViewType.None;
+    private NSViewController SubviewController = null;
+    private NSView Subview = null;
     
-    public MainWindow() : base("MainWindow", null)
+    public MainWindow() : base("MainWindow")
     {
     }
     
@@ -20,12 +23,39 @@ public partial class MainWindow : NSWindowController
 
         // Set the initial value for the label
         ClickedLabel.StringValue = "Button has not been clicked yet.";
+        
+        DisplaySubview(new SubViewTable(), SubViewType.TableView); //Display the table view
     }
     
     partial void ClickedButton (AppKit.NSButton sender) 
     {
-
         // Update counter and label
         ClickedLabel.StringValue = string.Format("The button has been clicked {0} time{1}.",++numberOfTimesClicked, (numberOfTimesClicked < 2) ? "" : "s");
+    }
+    
+    private void DisplaySubview(NSViewController controller, SubViewType type) 
+    {
+
+        // Is this view already displayed?
+        if (ViewType == type) return;
+
+        // Is there a view already being displayed?
+        if (Subview != null) {
+            // Yes, remove it from the view
+            Subview.RemoveFromSuperview ();
+
+            // Release memory
+            Subview = null;
+            SubviewController = null;
+        }
+
+        // Save values
+        ViewType = type;
+        SubviewController = controller;
+        Subview = controller.View;
+
+        // Define frame and display
+        Subview.Frame = new CGRect (0, 0, Window.ContentView.Frame.Width, Window.ContentView.Frame.Height);
+        Window.ContentView.AddSubview (Subview);
     }
 }
